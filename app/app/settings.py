376ba4eaf83f36324a7967a21f9132c9dcc558c3
Paloma Piot <paloma.piot@viesure.io>
2020-04-01 21:33:08 +0200

    Allowed hosts

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'p*b@bm8!21=qr$!jtxxp@7=-)im&*ex=ma$$$dte0w2h55ft=c'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['earlydetection-staging.herokuapp.com', 'earlydetection.herokuapp.com']
 
 
 # Application definition
