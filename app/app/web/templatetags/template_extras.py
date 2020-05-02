4068ee3af50fc863bcf90ea8f444cf759571ad88
Paloma Piot <piot.paloma@gmail.com>
2020-05-02 13:46:06 +0200

    Tooltip to popover on hover

--- app/app/web/templatetags/template_extras.py
+++ app/app/web/templatetags/template_extras.py
@@ -5,5 +5,4 @@ register = template.Library()
 @register.filter
 def reason(value, arg):
     """Filters the given list of reasons by arg type"""
-    print(value)
     return list(filter(lambda reason: reason['profile_data_type'] == arg, value))
